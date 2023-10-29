cebfc94faca04c69ba0f6a4012535a3601f80297
Kai Eugene <kai.yougene@gmail.com>
2023-10-29 12:33:29 +0900

    FEAT: Implement postgame component

--- client/src/Postgame.jsx
+++ client/src/Postgame.jsx
@@ -1,26 +1,30 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Homepage from "./Homepage";
+import { Game } from "./Game";
 
 const Postgame = ({ score }) => {
   const [leaderboard, setLeaderboard] = useState([]);
+  const [playAgain, setPlayAgain] = useState(false);
 
   const navigate = useNavigate();
 
-  //   useEffect(() => {
-  //     fetch("/leaderboard")
-  //       .then((res) => res.json())
-  //       .then((data) => setLeaderboard(data));
-  //   }, []);
+  useEffect(() => {
+    fetch("/leaderboard")
+      .then((res) => res.json())
+      .then((data) => setLeaderboard(data));
+  }, []);
 
   const handleHomeButton = () => {
     return navigate("/home");
   };
 
   const handleReplayButton = () => {
-    return navigate("/play");
+    return setPlayAgain(true);
   };
-  return (
+  return playAgain ? (
+    <Game />
+  ) : (
     <>
       <h1>Leaderboard</h1>
       <ul>
