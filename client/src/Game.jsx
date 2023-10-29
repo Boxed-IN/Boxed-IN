3796b95ed47c9f869c0754c16e053e9392881b78
vororo <adamolsson@outlook.com>
2023-10-29 17:49:58 +0900

    implemented shuffling of posters, working on zooming

--- client/src/Game.jsx
+++ client/src/Game.jsx
@@ -5,12 +5,14 @@ import { useState, useRef, useEffect } from "react";
 import Postgame from "./Postgame";
 
 export const Game = () => {
-  const [answer, setAnswer] = useState("Grime");
+  //const [answer, setAnswer] = useState("Grime");
   const [currentUser, setCurrentUser] = useState(null);
   const [score, setScore] = useState(0);
   const [timer, setTimer] = useState(30);
   const [movies, setMovies] = useState([{ poster_link: poster }]);
   const [isGameOver, setIsGameOver] = useState(false);
+  const startScale = 5;
+  const [scale, setScale] = useState(startScale);
 
   const answerInput = useRef("");
 
@@ -35,6 +37,7 @@ export const Game = () => {
       return;
     } else {
       const timeout = setTimeout(() => {
+        setScale((prevScale) => prevScale - 1);
         setTimer((prevTimer) => prevTimer - 1);
       }, 1000);
       return () => clearTimeout(timeout);
@@ -48,16 +51,38 @@ export const Game = () => {
   async function handleGetPosters() {
     let result = await fetch("/movieInfo");
     let parsedResult = await result.json();
-    console.log(parsedResult);
-    setMovies(parsedResult);
-    console.log(movies);
+    let shuffledParsedResult = shuffle(parsedResult);
+    setMovies(shuffledParsedResult);
+  }
+
+  function handleSwapPoster() {
+    answerInput.current.value = "";
+    setMovies(movies.slice(1));
+  }
+
+  function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
   }
 
   return isGameOver ? (
     <Postgame score={score} />
   ) : (
     <>
-      <img src={movies[0].poster_link}></img>
+      <img src={movies[0].poster_link} style={{transition: "transform 1000ms ease-in-out", transform: `scale(${scale})`}}></img>
       <p>Score: {score} </p>
       <h1> Timer: {timer} </h1>
       <input
@@ -67,12 +92,13 @@ export const Game = () => {
         onKeyDown={(e) => {
           console.log(e.key);
           if (e.key === "Enter") {
-            console.log(answerInput.current.value + "vs" + answer);
-            if (answerInput.current.value == answer) {
-              console.log("answered");
+            console.log(answerInput.current.value + " vs " + movies[0].title);
+            if (answerInput.current.value == movies[0].title) {
+              console.log("answered: " + movies[0].title);
               setScore((prevScore) => prevScore + 1);
+              handleSwapPoster()
             } else {
-              console.log("failed");
+              console.log("failed: " + movies[0].title);
               setScore((prevScore) => prevScore - 1);
             }
           }
